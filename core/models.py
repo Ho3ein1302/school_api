be5024648fe7ec4fd0adfba6a11ea71cc205b55c
Shinigami1302 <hosseinfatehi1302@gmail.com>
2023-11-25 21:36:58 +0330

    Add school model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.gis.db import models as model
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=300, db_index=True, verbose_name='name')
+    description = models.CharField(max_length=1000, blank=True, null=True, verbose_name='description')
+    address = models.TextField(verbose_name='address')
+    location = model.GeometryField(geography=True, verbose_name='location')
+
+    class Meta:
+        verbose_name = 'school'
+        verbose_name_plural = 'schools'
