c59f875276285c4908272a9ca5ab6a01be4fb1bf
Shinigami1302 <hosseinfatehi1302@gmail.com>
2023-11-26 20:47:32 +0330

    Add course model

--- core/models.py
+++ core/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.gis.db import models as model
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+from rest_framework.validators import ValidationError
 
 from .validators import check_landline_number
+from user.models import User
 
 
-class School(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated at')
+
+
+class School(BaseModel):
     name = models.CharField(max_length=300, db_index=True, verbose_name='name')
     description = models.CharField(max_length=1000, blank=True, null=True, verbose_name='description')
     landline_number = models.CharField(max_length=11, validators=[check_landline_number])
@@ -17,3 +27,19 @@ class School(models.Model):
     class Meta:
         verbose_name = 'school'
         verbose_name_plural = 'schools'
+
+
+class Course(BaseModel):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+        db_index=True,
+        verbose_name=_('Course name')
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Course')
+        verbose_name_plural = _('Courses')
