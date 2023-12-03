f2722185c3d3b5d64532e2902d4819edcc7f0d82
Ho3ein1302 <hosseinfatehi1302@gmail.com>
2023-12-03 20:07:54 +0330

    Add Swagger

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,11 @@
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
+# JWT Token
 def get_tokens(user) -> dict:
     """
         The function "get_tokens()" takes in "user" and returns a "dict".
@@ -12,3 +17,18 @@ def get_tokens(user) -> dict:
         "refresh": str(refresh),
         "access": str(refresh.access_token)
     }
+
+
+# Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="School API",
+      default_version='v1',
+      description="school API",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
--- school_api/urls.py
+++ school_api/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+from core.utils import schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/school/', include('core.urls')),
+    path('api/user/', include('user.urls')),
+    path('api/teacher/', include('user.teacher.urls')),
+    path('api/student/', include('user.student.urls')),
+
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
